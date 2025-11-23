6fef4dc3e52f504951457ccb0cceb931fd2448f1
Raul-Sorin Sorban <raulssorban@users.noreply.github.com>
2025-11-23 14:31:04 +0000

    Baseline

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+cd Carbon
+echo "Updating Carbon Codespaces environment..."
+dotnet run --project Carbon.Core/Carbon.Tools/Carbon.Runner Tools/Build/runners/bootstrap.cs
+dotnet run --project Carbon.Core/Carbon.Tools/Carbon.Runner Tools/Build/runners/update.cs staging
+
+cd ..
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y ca-certificates curl lib32gcc-s1
+sudo useradd -m steam || true
+sudo mkdir -p steamcmd
+sudo chown -r $USER:$USER steamcmd
+
+cd steamcmd
+steamCmd=https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+curl -sqL $steamCmd -o steamcmd_linux.tar.gz
+tar -xvzf steamcmd_linux.tar.gz
+rm steamcmd_linux.tar.gz
+sudo ln -sf steamcmd.sh /usr/local/bin/steamcmd
+
+./steamcmd.sh +force_install_dir "../Server" +login anonymous +app_update 258550 -beta staging validate +quit
+
+echo "Update complete."
